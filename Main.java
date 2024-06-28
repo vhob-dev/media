15826adbbbefec4d061884688550054506e4ec43
vhob-dev <dev.vhobtrabalhos@gmail.com>
2024-06-27 21:58:55 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        int nota1 = 30;
+        int nota2 = 50 ;
+        int media = nota1 + nota2;
+        System.out.println(media);
+    }
+}
